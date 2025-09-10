6c36145605ebb57ca310a0a959245ccfb9e03bc5
Thohorn <64076909+Thohorn@users.noreply.github.com>
2025-09-10 12:53:57 +0200

    JavaScript, Opdracht 7 Stap 1

---
+++ boodschappenlijst/script.js
@@ -0,0 +1,7 @@
+const groceryTable = document.getElementById('grocery-table');
+
+function updateTable() {
+    console.log('table updated');
+}
+
+groceryTable,addEventListener('change', updateTable);
