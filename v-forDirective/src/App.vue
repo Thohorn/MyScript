a8635a58aa849a2f1ae283e5d9c43a607724f321
Thohorn <64076909+Thohorn@users.noreply.github.com>
2025-09-25 11:21:09 +0200

    Hoofdstuk Vue, Opdracht 5 De v-for directive

---
+++ v-forDirective/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const tasks = ref([ { name: 'Boodschappen doen', completed: false },
+                      { name: 'Afwassen', completed: true },
+                      { name: 'Hond uitlaten', completed: false }]
+                    );
+
+  const changeCompleted = (task) => {
+    task.completed = !task.completed
+  }
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="({name, completed}, index) in tasks">{{ name }} {{ completed }} <button @click="changeCompleted(tasks[index])">af/niet af</button></li>
+  </ul>
+  
+</template>
+
+<style scoped>
+
+</style>
