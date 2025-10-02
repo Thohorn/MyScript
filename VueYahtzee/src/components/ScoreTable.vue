85ddb355706ff1e0063c57fe55797e0e9eb56eb4
Thohorn <64076909+Thohorn@users.noreply.github.com>
2025-10-02 11:55:00 +0200

    VueTahtzee Did some Part 1 completed. Part two WIP

--- VueYahtzee/src/components/ScoreTable.vue
+++ VueYahtzee/src/components/ScoreTable.vue
@@ -3,10 +3,60 @@
 
     const dice = defineModel();
 
-    const countOnes = computed(() => {
-        let ones = dice.value.filter((element) => {return element === 1})
+    const sumSingleValue = (singleValue) => {
+        let ones = dice.value.filter((element) => {return element === singleValue})
         return ones.reduce((sum, num) => sum + num, 0);
-    })
+    }
+
+    const sumOnes = computed(() => sumSingleValue(1));
+    const sumTwos = computed(() => sumSingleValue(2));
+    const sumThrees = computed(() => sumSingleValue(3));
+    const sumFours = computed(() => sumSingleValue(4));
+    const sumFives = computed(() => sumSingleValue(5));
+    const sumSixes = computed(() => sumSingleValue(6));
+
+    const sumOfDice = computed(() => dice.value.reduce((sum, num) => sum + num, 0));
+    const bonus = computed(() => sumOfDice.value > 63 ? 35 : 0);
+    const p1Total = computed(() => sumOfDice.value + bonus.value);
+
+    const checkXOfAKind = (targetCount, sortedDice = dice.value.sort((a, b) => a - b)) => {
+        if(dice.value[0] === 0){
+            return false;
+        }
+        let count = 1;
+        for (let i = 0; i < sortedDice.length; i++){
+            if (i+1 !== sortedDice.length){
+                if(sortedDice[i] === sortedDice[i+1]){
+                    count++;
+                    if (count === targetCount){
+                        return true;
+                    }
+                } else {
+                    count = 1;
+                }
+            }
+        }
+        return false;        
+    }
+
+    const checkFullHouse = () => {
+        let sortedDice = dice.value.sort((a, b) => a - b);
+        let threedice = sortedDice.slice(2);
+        let twodice = sortedDice.slice(0, 2);
+        if (checkXOfAKind(3, threedice) && checkXOfAKind(2, twodice)){
+            return true;
+        }
+        return false;
+            
+    }
+
+    const threeOfAKind = computed(() => checkXOfAKind(3) ? sumOfDice.value : 0);
+    const carre = computed(() => checkXOfAKind(4) ? sumOfDice.value : 0);
+    const Topscore = computed(() => checkXOfAKind(5) ? 50 : 0);
+
+    const fullHouse = computed(() => checkFullHouse() ? 25: 0);
+
+
 
 </script>
 
@@ -49,7 +99,7 @@
             <tr>
                 <td>Enen</td>
                 <td>Tel alle Enen</td>
-                <td class="first-game">{{ countOnes }}</td>
+                <td class="first-game">{{ sumOnes }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -59,7 +109,7 @@
             <tr>
                 <td>Tweeën</td>
                 <td>Tel alle Tweeën</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumTwos }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -69,7 +119,7 @@
             <tr>
                 <td>Drieën</td>
                 <td>Tel alle Drieën</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumThrees }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -79,7 +129,7 @@
             <tr>
                 <td>Vieren</td>
                 <td>Tel alle Vieren</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumFours }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -89,7 +139,7 @@
             <tr>
                 <td>Vijven</td>
                 <td>Tel alle Vijven</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumFives }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -99,7 +149,7 @@
             <tr>
                 <td>Zessen</td>
                 <td>Tel alle Zessen</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumSixes }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -109,7 +159,7 @@
             <tr>
                 <td>Totaal aantal punten</td>
                 <td>-></td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumOfDice }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -119,7 +169,7 @@
             <tr>
                 <td>Bonus</td>
                 <td>35 punten</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ bonus }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -129,7 +179,7 @@
             <tr>
                 <td>Totaal van de bovenste helft</td>
                 <td>-></td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ p1Total }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -145,7 +195,7 @@
             <tr>
                 <td>Three of a kind</td>
                 <td>Totaal v.d. 5 stenen</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ threeOfAKind }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -155,7 +205,7 @@
             <tr>
                 <td>Carré</td>
                 <td>Totaal v.d. 5 stenen</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ carre }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -165,7 +215,7 @@
             <tr>
                 <td>Full House</td>
                 <td>25 punten</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ fullHouse }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -195,7 +245,7 @@
             <tr>
                 <td>Topscore</td>
                 <td>50 punten</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ Topscore }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
@@ -205,7 +255,7 @@
             <tr>
                 <td>Chance</td>
                 <td>Totaal v.d. 5 stenen</td>
-                <td class="first-game">0</td>
+                <td class="first-game">{{ sumOfDice }}</td>
                 <td class="second-game">0</td>
                 <td class="third-game">0</td>
                 <td class="fourth-game">0</td>
