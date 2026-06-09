82d730f26defbb94f87399a470dc506ffc504012
Thohorn <64076909+Thohorn@users.noreply.github.com>
2026-06-09 12:26:29 +0200

    Setup of sanctum-authentication project

---
+++ projecten/sanctum-authentication/resources/js/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import { postRequest } from './services/http';
+
+    const user = ref({
+        name: "",
+        password: "",
+    });
+
+
+    const handleSubmit = () => {
+        alert("Logging in?");
+    }
+
+</script>
+
+<template>
+    <h1>Login</h1>
+    <form @submit.prevent="handleSubmit">
+        <label for="name" class="mr-4">Gebruikersnaam:</label>
+        <input id="name" name="name" v-model="user.name" required>
+
+        <label for="password" class="mr-4">Wachtwoord:</label>
+        <input id="password" name="password" type="password" v-model="user.password" required>
+
+        <button type="submit">Inloggen</button>
+    </form>
+</template>
